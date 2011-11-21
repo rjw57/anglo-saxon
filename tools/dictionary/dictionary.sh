46421fadd95fe6d39a5bb14188a0157b5ef4917d
Rich Wareham <rjw57@cam.ac.uk>
2011-11-21 09:16:59 +0000

    dictionary.sh: add some cross-platform sprinkles

--- tools/dictionary/dictionary.sh
+++ tools/dictionary/dictionary.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-(sleep 1; xdg-open http://localhost:8000/) &
 cd `dirname $0`
-python -m http.server
+
+case `uname` in
+  Darwin)
+    (sleep 1; open http://localhost:8000/) &
+    python -m SimpleHTTPServer
+    ;;
+  *)
+    (sleep 1; xdg-open http://localhost:8000/) &
+    if [ -z `which python3` ]; then
+      python -m SimpleHTTPServer
+    else
+      python3 -m http.server
+    fi
+    ;;
+esac
 
