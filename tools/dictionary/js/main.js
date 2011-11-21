2654af2df33b60029bc1d419534461e153d4c28e
Rich Wareham <rjw57@cam.ac.uk>
2011-11-21 15:36:43 +0000

    re-work dictionary tool to work with smaller JSON object

--- tools/dictionary/js/main.js
+++ tools/dictionary/js/main.js
@@ -7,51 +7,107 @@ $(document).ready(function() {
   });
 
   $('#search').bind('input', function() {
-    query = $('#search').attr('value').trim();
-    results = $('#search_results');
+    query = $('#search').attr('value').trim().toLowerCase();
+    results_elem = $('#search-results');
 
-    results.empty();
+    // Empty the results display
+    results_elem.empty();
+
+    // No query? We're done.
     if(query.length == 0) {
       return;
     }
 
-    words = []
-    for(word_id in dictionary.words) {
+    // Generate a list of objects representing the search results. Each object
+    // has a score attribute and id attribute.
+    results = [];
+    for(word_id in dictionary) {
       // Remove 'q's (macrons) from word ids
       norm_word_id = word_id.replace(/q/g, '');
-      if(0 === norm_word_id.indexOf(query)) {
-        words.push(word_id);
+
+      score = -1; // no match
+
+      // exact match?
+      if(norm_word_id == query) {
+        score = 100;
+      } else if(0 === norm_word_id.indexOf(query)) {
+        // partial match, score based on remaining letter count
+        score = 100 / (1 + norm_word_id.length - query.length);
+      } else {
+        // search entries
+        entries = dictionary[word_id].entries;
+        for(entry_idx in entries) {
+          entry_text = entries[entry_idx].toLowerCase();
+          if(-1 != entry_text.indexOf(query))
+          {
+            score = 50;
+          }
+        }
       }
+
+      // skip negative score
+      if(score < 0) {
+        continue;
+      }
+
+      results.push({'score': score, 'id': word_id });
     }
-    if(words.length > 300) {
+
+    if(results.length > 300) {
       return;
     }
 
-    words = words.sort(function(a,b) {
-      a = a.replace(/q/g, '');
-      b = b.replace(/q/g, '');
+    // sort by _ascending_ score and then normalised word if
+    results = results.sort(function(a,b) {
+      // sort by score
+      if(a.score !== b.score) {
+        return b.score - a.score;
+      }
 
-      if(a === b) {
+      // then by id
+      a_id = a.id.replace(/q/g, '');
+      b_id = b.id.replace(/q/g, '');
+      if(a_id === b_id) {
         return 0;
       }
-
-      return a < b ? -1 : 1;
+      return a_id < b_id ? -1 : 1;
     });
 
-    result_list = []
-    for(word_id_idx in words) {
-      word_id = words[word_id_idx];
+    for(result_idx in results) {
+      var result = results[result_idx];
+      var word_id = result.id;
 
-      entries = dictionary.words[word_id];
-      for(idx in entries) {
-        result_list.push(entries[idx]);
+      var head = dictionary[word_id].head;
+      var entries = dictionary[word_id].entries;
+
+      var result_elem = $('<div class="result">')
+      result_elem.append($('<div class="head">').text(head));
+
+      var entries_elem = $('<div class="entries">');
+      
+      if(entries.length > 1) {
+        var list_elem = $('<ol>');
+        for(idx in entries) {
+          list_elem.append($('<li>').html(entries[idx]));
+        }
+        entries_elem.append(list_elem)
+      } else {
+        entries_elem.append($('<p>').html(entries[0]));
       }
-    }
 
-    console.log('Search: ' + query + ' -> ' + result_list.length.toString() + ' results');
+      // filter any links to fill in the search box
+      entries_elem.find('a[href*="#word_"]').each(function() {
+        var target = $(this).attr('href').replace(/^.*#word_/, '');
+        target = target.replace(/_[ivx]+$/, '');
+        target = target.replace(/_/g, ' ');
+        $(this).attr('href', '#');
+        $(this).click(function() {
+          $('#search').attr('value', target).trigger('input');
+        });
+      });
 
-    for(idx in result_list) {
-      results.append(result_list[idx]);
+      result_elem.append(entries_elem);
+      results_elem.append(result_elem);
     }
   });
 
